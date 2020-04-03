960be6e4e36548ed5684f70203d97c64708df345
David Oliver <davooliver1964@gmail.com>
2020-04-03 09:57:38 -0400

    Updates

---
+++ precache-manifest.3dbe989aa87aff33b394ce45a1080c5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d410eaa9a786f4487070f6d46d27ae",
+    "url": "/w4-restaurant/index.html"
+  },
+  {
+    "revision": "4843a5917a4c1fe573b1",
+    "url": "/w4-restaurant/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "dfc86eab215c4ccf99cc",
+    "url": "/w4-restaurant/static/css/main.850fea9e.chunk.css"
+  },
+  {
+    "revision": "4843a5917a4c1fe573b1",
+    "url": "/w4-restaurant/static/js/2.c233a86d.chunk.js"
+  },
+  {
+    "revision": "15afbc28b03670244a700bdd5bb7139c",
+    "url": "/w4-restaurant/static/js/2.c233a86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc86eab215c4ccf99cc",
+    "url": "/w4-restaurant/static/js/main.82c2cdbc.chunk.js"
+  },
+  {
+    "revision": "34e24a3bf7a6a33f78ae",
+    "url": "/w4-restaurant/static/js/runtime-main.fd7aae3a.js"
+  }
+]);
